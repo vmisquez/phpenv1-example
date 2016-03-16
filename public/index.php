eea4d3f6730dbf2f5f1bbd253f9595d48824f7c3
vmisquez <vincent.misquez@yahoo.com>
2016-03-16 22:44:07 +0000

    second commit

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once '../vendor/autoload.php';
+echo "Hello World.\n";
+
+$array = [1, "apple", 2, "foo", "bar"];
+
+var_dump($array);
+dump($array);
+
+$database = new medoo([
+    'database_type' => 'sqlite',
+    'database_file' => '../storage/database.db'
+]);
+
+dump($database);
+
